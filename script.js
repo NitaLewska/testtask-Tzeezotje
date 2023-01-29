408a212b7e8fc559a545705a2c26224abeb08a19
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-01-30 01:42:14 +0300

    feat: add mobile version

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 var reviewsSwiper = new Swiper(".reviewsSwiper", {
-    navigation: {
-      nextEl: ".reviews .swiper-button-next",
-      prevEl: ".reviews .swiper-button-prev",
-      
-    },
-    
-    loop: true,
-  });
+  navigation: {
+    nextEl: ".reviews .swiper-button-next",
+    prevEl: ".reviews .swiper-button-prev",
+  },
 
-  var gallerySwiper = new Swiper(".gallerySwiper", {
-    navigation: {
-      nextEl: ".gallery .swiper-button-next",
-      prevEl: ".gallery .swiper-button-prev",
-      
-    },
-    
-    slidesPerView:2,
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true
-  });
+  loop: true,
+});
+
+var gallerySwiper = new Swiper(".gallerySwiper", {
+  navigation: {
+    nextEl: ".gallery .swiper-button-next",
+    prevEl: ".gallery .swiper-button-prev",
+  },
+  slidesPerView: 2,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+});
