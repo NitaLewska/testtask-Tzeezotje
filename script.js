b982e83827b377e8e17af299ffef33340173ae7a
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-01-30 02:17:42 +0300

    feat: modal with remodal

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ var gallerySwiper = new Swiper(".gallerySwiper", {
   centeredSlides: true,
   loop: true,
 });
+
+
+$(document).ready(function () {
+  $("form").submit(function () {
+      // Получение ID формы
+      var formID = $(this).attr('id');
+      // Добавление решётки к имени ID
+      var formNm = $('#' + formID);
+      $.ajax({
+          type: "POST",
+          url: 'mail.php',
+          data: formNm.serialize(),
+          success: function (data) {
+              // Вывод текста результата отправки
+              $(formNm).html(data); 
+          },
+          error: function (jqXHR, text, error) {
+              // Вывод текста ошибки отправки
+              $(formNm).html(error);         
+          }
+      });
+      return false;
+  });
+});
